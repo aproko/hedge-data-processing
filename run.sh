523b563aee87cc44d69ee469916742435cd4e99e
prokofieva <prokofieva@cs.columbia.edu>
2015-09-21 15:17:21 -0400

    Cleaned up run.sh script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ $# -ne 2 ]
         echo "The first argument must be the directory containing the input files; the second argument must be the number of sentences per HIT in our AMT datasheet"
         exit
 else
-#python clean_text.py "$1"
+    python clean_text.py "$1"
     python create_datasheet.py "$1" "$2"
     echo "Done"
 fi
