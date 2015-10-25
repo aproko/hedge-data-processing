5d7084aae5d83ddece35454b2e2fce98ad7af1d1
prokofieva <prokofieva@cs.columbia.edu>
2015-10-24 20:41:15 -0400

    Updated the files for creating datasheet for AMT.

--- create_datasheet.py
+++ create_datasheet.py
@@ -1,133 +1,207 @@
 from os import listdir
 from os.path import isfile, join
-#from nltk.stem import WordNetLemmatizer
-from nltk.stem.snowball import EnglishStemmer
+from nltk.stem import WordNetLemmatizer
 import csv
 import sys
 import codecs
+import random
+from bs4 import BeautifulSoup
+
 
 listoflines = []
+part_listoflines = []
+part_uncert = []
 uncert = []
 dict = {}
 multi_dict = {}
+total_dict = {}
 hedge_counts = {}
+gold_qs = []
 
-hRelDef = "create uncertainty in the commitment of the speaker to what they are saying (eg. 'I [think] (some fact)' as opposed to 'I [know] (some fact)')"
-hPropDef = "introduce uncertainty or ambiguity into the content of the utterance itself (i.e. the speaker/author is fully committed to what they are saying but there is something imprecise in the content itself)"
-hRelAttrDef = "imply that the speaker attributes information to some other source in order to downplay it OR to garner power for their statement instead of just committing to the statement themselves"
+#Input directory is where the cleaned up .Anno files are located
+#numSentPerHIT is how many questions are asked in each HIT (usually 10)
+#partitionNum is maximally how many instances of each hedge we want to appear the AMT dataset
+#totalEx is the total number of examples we want in the datasheet
 
 input_directory = sys.argv[1]
 numSentPerHIT = int(sys.argv[2])
+partitionNum = int(sys.argv[3])
+totalEx = int(sys.argv[4])
 
-#lemmatizer = WordNetLemmatizer()
-stemmer = EnglishStemmer()
+lemmatizer = WordNetLemmatizer()
 
 #Dictionary is in the format: hedge, type of hedge, definition of that type of hedge, the hedging definition of that word, the hedging example of that word, the non-hedging definition of that word, the non-hedging example of that word
-with codecs.open('dictionary.csv', 'rU', encoding='utf-8') as d:
-    reader = csv.reader(d)
-    next(reader, None)
-    for line in reader:
-        dict_stem = stemmer.stem(line[0])
-        dict[dict_stem] = [line[1], line[2], line[3], line[4], line[5], line[6]]
 
-with codecs.open('multiword_dict.csv', 'rU', encoding='utf-8') as md:
-    md_reader = csv.reader(md)
-    next(md_reader, None)
-    for md_line in md_reader:
-        multi_dict[md_line[0]] = [md_line[1], md_line[2], md_line[3], md_line[4], md_line[5], md_line[6]]
+def readIn(filename, mode):
+    with codecs.open(filename, 'rU', encoding='utf-8') as inputFile:
+        reader = csv.reader(inputFile)
+        next(reader, None)
+        for line in reader:
+            if mode == "gold":
+                gold_qs.append(line)
+            else:
+                entry = [line[1], line[2], line[3], line[4], line[5], line[6]]
+                if mode == "dict":
+                    dict_lemma = lemmatizer.lemmatize(line[0])
+                    dict[dict_lemma] = entry
+                elif mode == "multi":
+                    multi_dict[line[0]] = entry
 
-onlyfiles = [ f for f in listdir(input_directory) if (isfile(join(input_directory,f)) and ".toAnno.txt" in f and ".DS_Store" not in f) ]
+def findHedges(id, orig_sentence, sentence, mode, total_dict):
+    words = sentence.split()
+    
+    #If we're looking for multi-word hedges, we add all potential ngrams (up to 6) that could be in the sentence to the list of words
+    if mode == "multi":
+        dictionary = multi_dict.copy()
+        words = orig_sentence.split()
+        sentence_length = len(orig_sentence.split())
+        maxlength = min(6, sentence_length)
+        for start in range(sentence_length):
+            potential_multiword = words[start]
+            
+            for length in range(1, min(maxlength, sentence_length - start)):
+                potential_multiword = " ".join([potential_multiword, words[start+length]])
+                words.append(potential_multiword)
+    
+    elif mode == "single":
+        dictionary = dict.copy()
+        
+    multi_line = sentence
+                                
+    for word in words:
+        if mode == "single":
+            word = lemmatizer.lemmatize(word)
+                                
+        if word in dictionary:
+            entry = id + ",\"" + orig_sentence + "\"," + word + "," + ",".join(dictionary[word])
+            listoflines.append(entry)
+                            
+            #We replace all multi-word hedges with "0" in order to not identify potential single hedge words within multi-word expressions.
+            if mode == "multi":
+                multi_line = multi_line.replace(word, "0")
 
 
-for files in onlyfiles:
-    with codecs.open(join(input_directory,files), 'rU', encoding='utf-8') as inputFile:
-        for lines in inputFile:
-            lines = lines.replace("\n", "")
-            lines = lines.replace("\r", "")
+            if total_dict[word] > 0 and entry not in gold_qs and len(part_listoflines) < totalEx:
+                part_listoflines.append(entry)
+                total_dict[word] -= 1
+        
+            #We increment the count
+            if word in hedge_counts:
+                hedge_counts[word] += 1
+            else:
+                hedge_counts[word] = 1
+
+    return multi_line
+
+def output(filename, listToPrint, mode):
+    if mode == "defs":
+        header_defs="sentenceID0,sentence0,hedge0,hedgeType0,defHedgeType0,hedgingDef0,hedgingEx0,nonHedgeDef0,nonHedgeEx0,answer"
+            
+        for i in range(1,numSentPerHIT):
+            header_defs = header_defs + ",sentenceID" +str(i) + ",sentence"+ str(i)+",hedge"+str(i)+",hedgeType"+str(i)+",defHedgeType"+str(i)+",hedgingDef"+str(i)+",hedgingEx"+str(i)+",nonHedgeDef"+str(i)+",nonHedgeEx"+str(i)
+
+    elif mode == "uncert":
+        header_defs = "sentenceID0,sentence0"
+            
+        for x in range(1,numSentPerHIT):
+            header_defs = header_defs + ",sentenceID" + str(x) + ",sentence"+str(x)
+
+
+    with codecs.open(filename, 'w', encoding='utf-8') as out_defs:
+        out_defs.write(header_defs)
+        out_defs.write("\n")
+            
+        b = 0
+        temp_defs = ""
+        count = 0
+        gold = list(gold_qs)
+            
+        for item in listToPrint:
+            if count < (totalEx // numSentPerHIT):
+                if b == 0:
+                    if mode == "defs":
+                        temp_defs = ",".join(gold.pop())
+                    elif mode == "uncert":
+                        temp_defs = item
+                    b += 1
+                if b < numSentPerHIT and b > 0:
+                    temp_defs = temp_defs + "," + item
+                    b += 1
+                if b == numSentPerHIT:
+                    out_defs.write(temp_defs)
+                    out_defs.write("\n")
+                    b = 0
+                    count += 1
+        if b != 0:
+            out_defs.write(temp_defs)
+    
+    
+
+
+def partitionUncert():
+    while len(part_uncert) < totalEx:
+        temp_unc = random.choice(uncert)
+        while temp_unc in part_uncert:
+            temp_unc = random.choice(uncert)
+        part_uncert.append(temp_unc)
+
+        random_hedged = random.choice(part_listoflines)
+        temp_random_hedged = random_hedged.split(",")
+        temp_random_hedged_part = temp_random_hedged[0] + "," + temp_random_hedged[1]
+    
+        while temp_random_hedged_part in part_uncert:
+            random_hedged = random.choice(part_listoflines)
+            temp_random_hedged = random_hedged.split(",")
+            temp_random_hedged_part = temp_random_hedged[0] + "," + temp_random_hedged[1]
+        part_uncert.append(temp_random_hedged_part)
+
+
+
+def main():
+    
+    readIn('dictionary.csv', "dict")
+    readIn('multiword_dict.csv', "multi")
+    readIn('amt_check_questions.csv', "gold")
+    
+    onlyfiles = [ f for f in listdir(input_directory) if (isfile(join(input_directory,f)) and ".toAnno.txt" in f) ]
+
+#This lets us keep track of the number of instances of each word in our datasheet
+    joined_list = dict.keys() + multi_dict.keys()
+    total_dict = dict.fromkeys(joined_list, partitionNum)
+
+    for files in onlyfiles:
+    
+    #Files are in the format: sentenceID, sentence
+        soup = BeautifulSoup(open(join(input_directory, files)), "lxml")
+        paragraph = soup.get_text()
+        paragraph = paragraph.strip()
+        para_list = paragraph.split("\n")
+
+        for lines in para_list:
             id_and_line = lines.split(",")
             line = id_and_line[1]
             id = id_and_line[0]
-            if len(line.split(" ")) > 3:
-                
-                #For the uncertainty determination and confidence level datasheet, we just need the sentence to be annotated and the 3 different definitions of hedging
+            
+            #Checking again that sentences are at least 3 words long.
+            if len(line.split()) > 2:
+                #For the uncertainty determination and confidence level datasheet, we just need the sentence which will be annotated
                 uncert.append(id + ",\""+line + "\"")
-                
-                multi_line = line
-                
-                for multi_words in multi_dict:
-                    multi_words_in_line = []
-                    index = 0
-                    if multi_words in line:
-                        multi_line = line.replace(multi_words, str(index))
+                multi_line = findHedges(id, line, line, "multi", total_dict)
+                temp_str = findHedges(id, line, multi_line, "single", total_dict)
 
-                        listoflines.append(id + ",\"" + line + "\"," + multi_words + "," + comma.join(multi_dict[multi_words]))
-                        
-                        multi_words_in_line.append(multi_words)
-                        index = index + 1
-                        if multi_words in hedge_counts:
-                            hedge_counts[multi_words] = hedge_counts[multi_words] + 1
-                        else:
-                            hedge_counts[multi_words] = 1
-            
-                words = multi_line.split(" ")
-            
-                for word in words:
-                    word_stem = stemmer.stem(word)
-                    if word_stem in dict:
-                        comma = ","
-                        
-                        #For the task identifying hedge senses based on word definitions and examples, we need the sentence, the potential hedge word and all the available information from the dictionary
-                        def_str = id + ",\"" + line + "\"," + word + "," + comma.join(dict[word_stem])
-                        listoflines.append(def_str)
-                        if word_stem in hedge_counts:
-                            hedge_counts[word_stem] = hedge_counts[word_stem] + 1
-                        else:
-                            hedge_counts[word_stem] = 1
+    if totalEx < len(uncert):
+        partitionUncert()
 
-#To be used for task asking about uncertainty present in the sentence and for confidence level determination
-#Header is: hRelDef, hPropDef, hRelAttrDef, sentence0, sentence1, ...., sentence(#numSentPerHIT)
-with codecs.open('amt_input_uncert.csv', 'w', encoding='utf-8') as out:
-    a = 0
-    header = "hRelDef,hPropDef,hRelAttrDef,"
-    for x in range(0,numSentPerHIT):
-        header = header + "sentenceID" + str(x) + ",sentence"+str(x)+","
-    out.write(header)
-    out.write("\n")
-    for item_u in uncert:
-        if a == 0:
-            temp = hRelDef + "," + hPropDef + "," + hRelAttrDef +"," + item_u
-            a = a+1
-        elif a < numSentPerHIT and a > 0:
-            temp = temp + "," + item_u
-            a = a+1
-        else:
-            out.write(temp)
-            out.write("\n")
-            a = 0
-    out.write(temp)
 
-#To be used to task asking turkers to identify hedge senses of words based on definitions alone, examples alone, or a combination of the two
-with codecs.open('amt_input_defs_examples.csv', 'w', encoding='utf-8') as out_defs:
-    header_defs="sentenceID0,sentence0,hedge0,hedgeType0,defHedgeType0,hedgingDef0,hedgingEx0,nonHedgeDef0,nonHedgeEx0"
-    for i in range(1,numSentPerHIT):
-        header_defs = header_defs + ",sentenceID" +str(i) + ",sentence"+ str(i)+",hedge"+str(i)+",hedgeType"+str(i)+",defHedgeType"+str(i)+",hedgingDef"+str(i)+",hedgingEx"+str(i)+",nonHedgeDef"+str(i)+",nonHedgeEx"+str(i)
-    out_defs.write(header_defs)
-    out_defs.write("\n")
-    b = 0
-    for item in listoflines:
-        if b == 0:
-            temp_defs = item
-            b = b + 1
-        elif b < numSentPerHIT and b > 0:
-            temp_defs = temp_defs + "," + item
-            b = b + 1
-        else:
-            out_defs.write(temp_defs)
-            out_defs.write("\n")
-            b = 0
-    out_defs.write(temp_defs)
+    output('amt_input_defs_examples_lem.csv', listoflines, "defs")
+    output('amt_input_uncert_lem.csv', uncert, "uncert")
+    output('amt_input_defs_examples_lem_PART.csv', part_listoflines, "defs")
+    output('amt_input_uncert_lem_PART.csv', part_uncert, "uncert")
 
-with codecs.open('amt_hedge_counts.csv', 'w', encoding='utf-8') as data_out:
-    for hedge, counts in hedge_counts.items():
-        data_out.write(",".join([hedge, str(counts)]))
-        data_out.write("\n")
+    with codecs.open('amt_hedge_counts.csv', 'w', encoding='utf-8') as data_out:
+        for hedge, counts in hedge_counts.items():
+            data_out.write(",".join([hedge, str(counts)]))
+            data_out.write("\n")
+
+main()
+
--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!bin/bash
 
+
     while getopts 'cdna*' flag; do
         case "${flag}" in
+            #$2 is the directory with the text files in .cmp.txt format
             c) python clean_text.py "$2"; echo "Cleaning Input Text" ;;
-            d) python create_datasheet.py "$2" "$3"; echo "Creating Datasheet" ;;
+            #$2 is the directory with the text files in .toAnno.txt format
+            #$3 is the number of sentences per HIT to be put in the datasheet
+            #$4 is the max number of times a hedge should appear in the dataset
+            #$5 is the total number of sentence we want to use
+            d) python create_datasheet.py "$2" "$3" "$4" "$5"; echo "Creating Datasheet" ;;
             n) python data_analysis.py "$2" "$3"; echo "Analyzing $3 data" ;;
-            a) python clean_text.py "$2"; python create_datasheet.py "$2" "$3"; echo "Running the entire pipeline" ;;
+            a) python clean_text.py "$2"; python create_datasheet.py "$2" "$3" "$4" "$5"; echo "Running the entire pipeline" ;;
         esac
     done
 
